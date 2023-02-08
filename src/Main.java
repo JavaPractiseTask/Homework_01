13ea29c06c695bfaba94559440a69b5ef8a23489
sergey.rikhter@outlook.com <sergey.rikhter@outlook.com>
2023-02-08 23:36:47 +0200

    Complete Task 05

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import Task.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
@@ -11,8 +13,16 @@ public class Main {
          System.out.printf(task.GetPercent());
 
          Task_03 task = new Task_03(3);
+
          System.out.printf("Generate integer from string : " + task.GenerateInt());
+         Task_04 task = new Task_04();
+
          */
-        Task_04 task = new Task_04();
+        System.out.printf("Press 0 for exit");
+         do {
+            var keyboard= new Scanner(System.in);
+            System.out.print("Entry month number: ");
+            Task_05 task = new Task_05(keyboard.nextInt());
+        }while( true);
     }
 }
--- src/Task/Task_05.java
+++ src/Task/Task_05.java
@@ -1,4 +1,38 @@
 package Task;
 
-public class Task_05 {
-}
+public class Task_05
+{
+    public Task_05(int number)
+    {
+            switch (number) {
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                case 2:
+                    System.out.printf("It's a winter");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.printf("It's a Spring");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.printf("It's a Summer");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.printf("It's an Autumn");
+                    break;
+                case 12:
+                    System.out.printf("It's a Winter");
+                    break;
+                default:
+                    System.out.printf("Incorrect month");
+                    break;
+            }
+        }
+    }
