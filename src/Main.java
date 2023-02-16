27d7c293c635ab017147e8e99aacfe265927e01d
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2023-02-16 16:23:31 +0200

    Complete task 11

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
+        /*
             Task_10 task = new Task_10();
             task.run();
+
+         */
+        Task_11 task = new Task_11();
+        task.run();
     }
 }
--- src/Task/Task_11.java
+++ src/Task/Task_11.java
@@ -1,4 +1,32 @@
 package Task;
 
-public class Task_11 {
+import java.util.Scanner;
+
+public class Task_11 implements Runnable {
+    public void DrawLeft()
+    {
+           System.out.println("Left ←");
+    }
+
+    public void DrawRight()
+    {       System.out.println("Right →");
+    }
+
+    public void DrawBottom()
+    {
+             System.out.println("Bottom ↓ ");
+    }
+
+    public void DrawTop()
+    {       System.out.println("Top ↑");
+    }
+    @Override
+    public void run() {
+       DrawLeft();
+       DrawRight();
+       DrawBottom();
+       DrawTop();
+    }
+
+
 }
