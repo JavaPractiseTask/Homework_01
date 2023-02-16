d18c759dd490e27dc2dd4019d89094cca06abfbb
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2023-02-16 16:27:57 +0200

    Add comment row

--- src/Task/Task_11.java
+++ src/Task/Task_11.java
@@ -3,30 +3,47 @@ package Task;
 import java.util.Scanner;
 
 public class Task_11 implements Runnable {
+    /**
+     * Show left
+     */
     public void DrawLeft()
     {
-           System.out.println("Left ←");
+        System.out.println("Left ←");
     }
 
+    /**
+     * Show right
+     */
     public void DrawRight()
-    {       System.out.println("Right →");
+    {
+        System.out.println("Right →");
     }
 
+    /**
+     * Show bottom
+     */
     public void DrawBottom()
     {
-             System.out.println("Bottom ↓ ");
+        System.out.println("Bottom ↓ ");
     }
 
+    /**
+     * Show top
+     */
     public void DrawTop()
-    {       System.out.println("Top ↑");
+    {
+        System.out.println("Top ↑");
     }
+
+    /**
+     * Running
+     */
     @Override
-    public void run() {
+    public void run()
+    {
        DrawLeft();
        DrawRight();
        DrawBottom();
        DrawTop();
     }
-
-
 }
